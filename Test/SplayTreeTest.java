3ef3275782bbe1777e92d0c6436cbc38bda2c9bd
unclepete-20 <pedroparty310@gmail.com>
2021-05-12 15:30:44 -0600

    Se realizaron las pruebas unitarias para el SplayBST.java

---
+++ Test/SplayTreeTest.java
@@ -0,0 +1,41 @@
+/**
+ * @author unclepete-20 y Jack200133
+ * Carnet 20188 y 20593
+ * Universidad del Valle de Guatemala
+ * Algoritmos y estructuras de datos
+ * Seccion 10
+ * Hoja de trabajo 9
+ */
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SplayTreeTest {
+
+    @Test
+    void add() {
+        SplayBST rbt = new SplayBST();
+        rbt.add("primero",1);
+        rbt.add("segun",2);
+        rbt.add("tercer",3);
+        rbt.add("cuarto",4);
+        rbt.add("quinto",5);
+        rbt.add("seis",6);
+        assert (rbt.contains("primero"));
+
+    }
+
+    @Test
+    void get() {
+        SplayBST rbt = new SplayBST();
+        rbt.add("primero",1);
+        rbt.add("segun",2);
+        rbt.add("tercer",3);
+        rbt.add("cuarto",4);
+        rbt.add("quinto",5);
+        rbt.add("seis",6);
+        assert (rbt.get("primero").equals(1));
+
+    }
+}

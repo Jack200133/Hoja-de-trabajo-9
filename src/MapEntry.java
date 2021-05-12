bee0258f48e8601a0977353315dfa800918443ee
Jack200133 <car20593@uvg.edu.gt>
2021-05-11 21:08:20 -0600

    Interfaz MapEntrry Para el Association

---
+++ src/MapEntry.java
@@ -0,0 +1,19 @@
+// interfas parea la calse de asociaciones
+public interface MapEntry<K,V> {
+    public boolean equals (Object o);
+    // post: returns true iff this entry <K,V> is equal to object o
+
+    public K getKey();
+    // post: returns the key K of this entry
+
+    public V getValue();
+    // post: returns the value V of this entry
+
+    public int hashCode();
+    // post: returns the hashcode for the key
+
+    public V setValue(V value);
+    // post: replaces the Value of this entry
+
+
+}
